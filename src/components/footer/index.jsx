664d58197725fa46778fa977f209062331633911
Luiz Guilherme <lguidossantos29@gmail.com>
2024-01-12 11:18:24 -0300

    merge

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { FooterContainer } from './styles'
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+        <h3>Desvendando segredos do mundo, uma curiosidade de cada vez. Conecte-se ao incrível!</h3>
+        <p>Strange World</p>
+
+
+        
+
+    </FooterContainer>
+  )
+}
+
+export default Footer
